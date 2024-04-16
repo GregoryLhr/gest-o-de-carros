e311a7b26d66ccb47012fd0e36e5b87bf18fca93
Gregory Ruan <gregorysouzagr@gmail.com>
2024-04-16 14:17:50 -0300

    mostrando carros no browser

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from cars.models import Car
 
 # Create your views here.
 
 def cars_view(request):
-    return HttpResponse('Meus carros')
+    cars = Car.objects.all()
+
+    return render(
+        request, 
+        'cars.html',
+        {'cars': cars}
+    )
