024a177dbb8d384e258d09e5b0e800fceb88b7e8
Gregory Ruan <gregorysouzagr@gmail.com>
2024-05-08 15:30:27 -0300

    integrando open ai para geração de bio automatica

--- openai_api/client.py
+++ openai_api/client.py
@@ -1,14 +1,14 @@
-# import openai
+import openai
 
-# def get_car_ai_bio(model, brand, year):
-#     prompt = '''
-#     me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo de carro.
-#     '''
-#     openai.api_key = '...'
-#     prompt = prompt.format(brand, model, year)
-#     response = openai.completions.create(
-#         model='text-davinci-003',
-#         prompt=prompt,
-#         max_tokens=1000,
-#     )
-#     return response['choices'][0]['text']
+def get_car_ai_bio(model, brand, year):
+    prompt = '''
+    me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo de carro.
+    '''
+    openai.api_key = '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'
+    prompt = prompt.format(brand, model, year)
+    response = openai.completions.create(
+        model='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1000,
+    )
+    return response['choices'][0]['text']
